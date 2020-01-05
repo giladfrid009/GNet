4ac71d90976303b1bd2aec9e95bd21bc7adc17e7
Gilad Freidkin <freidkin.gilad@gmail.com>
2020-01-05 22:13:37 +0200

    bug fix. added new T this[int index] in IShapedArray to avoid calling T this[params int[] indices] when haveing only 1 argument

--- GNet/DataStructure/IShapedArray.cs
+++ GNet/DataStructure/IShapedArray.cs
@@ -3,6 +3,7 @@
     public interface IShapedArray<T> : IArray<T>
     {
         Shape Shape { get; }
+        new T this[int index] { get; }
         T this[params int[] indices] { get; }
     }
 }
