780f0422499871dea0ba639f1c83d031a0e21aa3
Gilad Freidkin <freidkin.gilad@gmail.com>
2020-01-05 22:18:20 +0200

    small changes

--- GNet/Datasets/LogicGates.cs
+++ GNet/Datasets/LogicGates.cs
@@ -2,58 +2,58 @@
 {
     public static class LogicGates
     {
-        public static Dataset AND { get; } = new Dataset(new Data[]
-        {
+        public static Dataset AND { get; } = new Dataset
+        (        
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 0.0, 0.0), new ShapedArrayImmutable<double>(new Shape(1), 0.0)),
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 0.0, 1.0), new ShapedArrayImmutable<double>(new Shape(1), 0.0)),
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 1.0, 0.0), new ShapedArrayImmutable<double>(new Shape(1), 0.0)),
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 1.0, 1.0), new ShapedArrayImmutable<double>(new Shape(1), 1.0))
-        });
+        );
 
-        public static Dataset OR { get; } = new Dataset(new Data[]
-        {
+        public static Dataset OR { get; } = new Dataset
+        (
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 0.0, 0.0), new ShapedArrayImmutable<double>(new Shape(1), 0.0)),
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 0.0, 1.0), new ShapedArrayImmutable<double>(new Shape(1), 1.0)),
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 1.0, 0.0), new ShapedArrayImmutable<double>(new Shape(1), 1.0)),
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 1.0, 1.0), new ShapedArrayImmutable<double>(new Shape(1), 1.0))
-        });
+        );
 
-        public static Dataset XOR { get; } = new Dataset(new Data[]
-        {
+        public static Dataset XOR { get; } = new Dataset
+        (
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 0.0, 0.0), new ShapedArrayImmutable<double>(new Shape(1), 0.0)),
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 0.0, 1.0), new ShapedArrayImmutable<double>(new Shape(1), 1.0)),
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 1.0, 0.0), new ShapedArrayImmutable<double>(new Shape(1), 1.0)),
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 1.0, 1.0), new ShapedArrayImmutable<double>(new Shape(1), 0.0))
-        });
+        );
 
-        public static Dataset NAND { get; } = new Dataset(new Data[]
-        {
+        public static Dataset NAND { get; } = new Dataset
+        (
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 0.0, 0.0), new ShapedArrayImmutable<double>(new Shape(1), 1.0)),
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 0.0, 1.0), new ShapedArrayImmutable<double>(new Shape(1), 1.0)),
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 1.0, 0.0), new ShapedArrayImmutable<double>(new Shape(1), 1.0)),
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 1.0, 1.0), new ShapedArrayImmutable<double>(new Shape(1), 0.0))
-        });
+        );
 
-        public static Dataset NOR { get; } = new Dataset(new Data[]
-        {
+        public static Dataset NOR { get; } = new Dataset
+        (
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 0.0, 0.0), new ShapedArrayImmutable<double>(new Shape(1), 1.0)),
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 0.0, 1.0), new ShapedArrayImmutable<double>(new Shape(1), 0.0)),
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 1.0, 0.0), new ShapedArrayImmutable<double>(new Shape(1), 0.0)),
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 1.0, 1.0), new ShapedArrayImmutable<double>(new Shape(1), 0.0))
-        });
+        );
 
-        public static Dataset XNOR { get; } = new Dataset(new Data[]
-        {
+        public static Dataset XNOR { get; } = new Dataset
+        (
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 0.0, 0.0), new ShapedArrayImmutable<double>(new Shape(1), 1.0)),
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 0.0, 1.0), new ShapedArrayImmutable<double>(new Shape(1), 0.0)),
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 1.0, 0.0), new ShapedArrayImmutable<double>(new Shape(1), 0.0)),
             new Data(new ShapedArrayImmutable<double>(new Shape(2), 1.0, 1.0), new ShapedArrayImmutable<double>(new Shape(1), 1.0))
-        });
+        );
 
-        public static Dataset NOT { get; } = new Dataset(new Data[]
-        {
+        public static Dataset NOT { get; } = new Dataset
+        (
             new Data(new ShapedArrayImmutable<double>(new Shape(1), 0.0), new ShapedArrayImmutable<double>(new Shape(1), 1.0)),
             new Data(new ShapedArrayImmutable<double>(new Shape(1), 1.0), new ShapedArrayImmutable<double>(new Shape(1), 0.0))
-        });
+        );
     }
 }
